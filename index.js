b36533332965b1f5cddc5b5f59d857a3debc6ae3
Emery Tate <tateejt@gmail.com>
2020-01-31 04:22:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function displayMessage(msg) {
 
 /* Write your implementation of greet() */
 function greet(timeString) {
-  const hour = parseInt(timeString, 10)
+  const hour = parseInt(timeString, 10);
   if ( hour < 12) return "Good Morning"
   if ( hour > 17) return "Good Evening"
   return "Good Afternoon"
