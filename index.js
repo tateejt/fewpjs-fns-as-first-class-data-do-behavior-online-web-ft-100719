540514418b40c7f95e94a29cd87bf8e93fcf08cf
Emery Tate <tateejt@gmail.com>
2020-01-31 04:16:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
-function greet(string) {
+function greet(timeString) {
   const = 
 }
 /* Write your implementation of displayMessage() */
